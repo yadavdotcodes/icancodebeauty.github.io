c7cef601ff6ceca8f8b99afb0c53696c1a1325d1
icancodebeauty <admin@itsjava.ml>
2021-10-22 16:41:59 +0000

    Updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Tak from '../tak.mp3';
 
 function Body() {
-    const tak = new Audio(Tak);
     const handleOnLeave = () => {
         document.title = "Aditya Yadav";
     }
@@ -12,9 +10,9 @@ function Body() {
                 <h1 className="big text">Aditya Yadav</h1>
                 <hr className="hr" />
                 <div className="text-center icon">
-                    <a onMouseEnter={() => {document.title = "Github"; tak.play();}} onMouseLeave={handleOnLeave} href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
-                    <a onMouseEnter={() => {document.title = "Instagram"; tak.play();}} onMouseLeave={handleOnLeave} href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
-                    <a onMouseEnter={() => {document.title = "Youtube"; tak.play();}} onMouseLeave={handleOnLeave} href="https://www.youtube.com/channel/UCb2cVlV_Ma7IIErZdYNNRKw" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Github";}} onMouseLeave={handleOnLeave} href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Instagram";}} onMouseLeave={handleOnLeave} href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Youtube";}} onMouseLeave={handleOnLeave} href="https://www.youtube.com/channel/UCb2cVlV_Ma7IIErZdYNNRKw" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
                 </div>
             </div>
         </div>
