57a43b5a8a0c2869d69411befd632fcdb3f8209d
icancodebeauty <admin@itsjava.ml>
2021-10-22 12:20:32 +0000

    Made My landing Page

---
+++ src/components/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div id="particles-js">
+            <div className="position-absolute top-50 start-50 translate-middle">
+                <h1 className="big text">Aditya Yadav</h1>
+                <hr className="hr" />
+                <div className="text-center icon">
+                    <a href="https://github.com" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
+                    <a href="https://github.com" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
+                    <a href="https://github.com" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body
