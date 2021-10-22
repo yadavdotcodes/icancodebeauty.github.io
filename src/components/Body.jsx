f592321998aaeb142a14b95e21bb40e672a62ee9
icancodebeauty <admin@itsjava.ml>
2021-10-22 16:33:18 +0000

    Added Music

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Tak from '../tak.mp3';
 
 function Body() {
+    const tak = new Audio(Tak);
+    const handleOnLeave = () => {
+        document.title = "Aditya Yadav";
+    }
     return (
         <div id="particles-js">
             <div className="position-absolute top-50 start-50 translate-middle">
                 <h1 className="big text">Aditya Yadav</h1>
                 <hr className="hr" />
                 <div className="text-center icon">
-                    <a href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
-                    <a href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
-                    <a href="https://www.youtube.com/channel/UCb2cVlV_Ma7IIErZdYNNRKw" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Github"; tak.play();}} onMouseLeave={handleOnLeave} href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Instagram"; tak.play();}} onMouseLeave={handleOnLeave} href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
+                    <a onMouseEnter={() => {document.title = "Youtube"; tak.play();}} onMouseLeave={handleOnLeave} href="https://www.youtube.com/channel/UCb2cVlV_Ma7IIErZdYNNRKw" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
                 </div>
             </div>
         </div>
