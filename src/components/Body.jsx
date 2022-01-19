244c953406584fb17fae51dcfba841700723c7ca
icancodebeauty <adi@mail.io>
2022-01-19 16:31:07 +0000

    Added Twitter And Mail

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -10,9 +10,10 @@ function Body() {
                 <h1 className="big text text-center text-wrap">Aditya Yadav</h1>
                 <hr className="hr" />
                 <div className="text-center icon">
-                    <a onMouseEnter={() => { document.title = "Github"; }} onMouseLeave={handleOnLeave} href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
-                    <a onMouseEnter={() => { document.title = "Instagram"; }} onMouseLeave={handleOnLeave} href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
-                    <a onMouseEnter={() => { document.title = "Youtube"; }} onMouseLeave={handleOnLeave} href="https://www.youtube.com/channel/UCb2cVlV_Ma7IIErZdYNNRKw" target="_blank" rel="noreferrer" ><i className="icon bi bi-youtube mx-3"></i></a>
+                    <a onMouseEnter={() => { document.title = "Github!"; }} onMouseLeave={handleOnLeave} href="https://github.com/dedomil" target="_blank" rel="noreferrer" ><i className="icon bi bi-github mx-3"></i></a>
+                    <a onMouseEnter={() => { document.title = "Instagram!"; }} onMouseLeave={handleOnLeave} href="https://instagram.com/codewithaadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-instagram mx-3"></i></a>
+                    <a onMouseEnter={() => { document.title = "Twitter!"; }} onMouseLeave={handleOnLeave} href="https://twitter.com/CodeWithAadi" target="_blank" rel="noreferrer" ><i className="icon bi bi-twitter mx-3"></i></a>
+                    <a onMouseEnter={() => { document.title = "Mail Me! :)"; }} onMouseLeave={handleOnLeave} href="mailto:aditya@aditya.is-a.dev" target="_blank" rel="noreferrer" ><i className="icon bi bi-envelope-fill mx-3"></i></a>
                 </div>
             </div>
         </div>
