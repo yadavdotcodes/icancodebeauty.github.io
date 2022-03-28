8e1c8ff9b0cc99c217bf829ae09dbffac11f3fea
icancodebeauty <adi@mail.io>
2022-03-28 06:16:18 +0000

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <><Navbar /><Component {...pageProps} /></>
+  return <>
+    <Navbar />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
