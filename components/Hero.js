088ac7c0a593d8f47996a08de0cb8e573e7d3eff
icancodebeauty <adi@mail.io>
2022-03-28 06:19:49 +0000

    Resolved Errors

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
                 <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://i.imgur.com/ItaLBc7.gif" />
                 <div className="text-center lg:w-2/3 w-full">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-base-content">Aditya Yadav 👨‍💻</h1>
-                    <p className="mb-8 leading-relaxed text-base-content">A Nerd, A Programmer, A Dreamer coz. Something That Doesn't Work In Real Works In Dream So. 😉</p>
+                    <p className="mb-8 leading-relaxed text-base-content">A Nerd, A Programmer, A Dreamer coz.Something That Doesn&apos;t Work In Real Works In Dream So. 😉</p>
                     <div className="flex justify-center">
                         <button className="btn btn-primary mx-2">BLog</button>
                         <button className="btn btn-primary mx-2">Contact</button>
--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,12 @@ export default function Navbar() {
             </div>
             <div className="flex-none">
                 <div className="dropdown dropdown-end">
-                    <label tabindex="0" className="btn btn-ghost btn-circle">
+                    <label tabIndex="0" className="btn btn-ghost btn-circle">
                         <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLineJoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                         </div>
                     </label>
-                    <div tabindex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                    <div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
                         <div className="card-body">
                             <div className="card-actions">
                                 <button className="btn btn-primary btn-block">Projects</button>
